07bcdf7a15eb5dfbdcd394f17a5d1e8d1b5f6a30
kavindadilshan <kavindadilshan97@gmail.com>
2023-08-12 00:32:36 +0530

    ui changes

--- src/constance/Constance.js
+++ src/constance/Constance.js
@@ -2,3 +2,39 @@ export const BASE_URL =  "/admin";
 export const CLIENT_NAME = 'admin';
 export const CLIENT_SECRET = '';
 export const USER_ROLE_01 ='partneraussie';
+
+export const DesignationTypes = [
+  {label:'Mobile Developer',value:1},
+  {label:'QA Engineer',value:2},
+  {label:'Project Manager',value:3},
+  {label:'Business Analyst',value:4},
+  {label:'UI / UX Engineer',value:5},
+  {label:'Frontend Web Developer',value:6},
+  {label:'Backend Developer',value:7},
+  {label:'Supervisor',value:8},
+  {label:'Full Stack Developer',value:9},
+  {label:'Project Manager',value:10},
+  {label:'Sales Manager',value:11}
+]
+
+export const ProjectsList = [
+  {label:'Edulab', value:1},
+  {label:'LECO', value:2},
+  {label:'VisitorTag', value:3},
+]
+
+
+export const TaskType = [
+  {label:'Bug Fixing',value:1},
+  {label:'Developments',value:2},
+  {label:'Support',value:3},
+  {label:'Learning',value:4},
+  {label:'Knowledge Sharing',value:5},
+  {label:'Documentation',value:6},
+  {label:'Client Meeting',value:7},
+  {label:'Project Meeting',value:8},
+  {label:'Client Meeting',value:9},
+  {label:'UI / UX',value:10},
+  {label:'Designing',value:11},
+  {label:'Research',value:12},
+]
--- src/views/AddTask/AddTask.js
+++ src/views/AddTask/AddTask.js
@@ -22,7 +22,7 @@ import {
   Form
 } from "reactstrap";
 import {Link} from "react-router-dom";
-import {BASE_URL} from "../../constance/Constance";
+import {BASE_URL, DesignationTypes, ProjectsList, TaskType} from "../../constance/Constance";
 import './AddTask.scss';
 import * as ProductService from '../../services/product';
 import * as CommonFunc from '../../utils/CommonFunc';
@@ -30,7 +30,6 @@ import Loader from "../../components/Loader/loading";
 import {StorageStrings} from "../../constance/StorageStrings";
 import swal from "sweetalert";
 import {AppSwitch} from "@coreui/react";
-import Dropzone from "react-dropzone";
 import * as Validations from "../../validation/Validation";
 
 class AddTask extends Component {
@@ -59,79 +58,27 @@ class AddTask extends Component {
     asSearch: false,
     editEnabled: false,
 
-    drop: true,
-    src: null,
-    imgBase64: '',
-    asImageEdit: false,
-    dropdownOpen: false,
-    productId: '',
-    productName: '',
+    taskId: '',
+    date:new Date(),
     description: '',
-    unitPrice: '',
-    selectedCategory: '',
-    qty: 0,
-    status: false,
-    category: [],
+    hours: '',
+    minutes:'',
+    comment:'',
+
+    taskType: TaskType,
+    projects: ProjectsList,
+    designationTypes: DesignationTypes,
+    mainFeatures: [],
+    selectedTaskType: '',
+    selectedProject: '',
+    selectedDesignation:'',
+    selectedMainFeature: '',
   }
 
   async componentDidMount() {
-    // await this.getAllCategories();
-    // await this.getAllProducts();
+
   }
 
-  getAllCategories = async () => {
-    await ProductService.getAllCategory()
-      .then(response => {
-        let list = [];
-        if (response.success) {
-          response.data.map((items) => {
-            list.push({
-              label: items.name,
-              value: items.id
-            })
-          })
-          this.setState({category: list})
-        } else {
-          CommonFunc.notifyMessage(response.message);
-        }
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  getAllProducts = async () => {
-
-    this.setState({loading: true})
-    await ProductService.getAllProduct()
-      .then(response => {
-        let list = [];
-
-        if (response.success) {
-
-          response.data.map((items) => {
-            list.push({
-              productId: items.id,
-              productName: items.name,
-              description: items.description,
-              status: items.status,
-              unitPrice: items.price,
-              image: items.imageUrl,
-              category: items.categoryId,
-              qty: items.qty
-            })
-          });
-          this.setState({list: list, loading: false});
-        } else {
-          CommonFunc.notifyMessage(response.message);
-          this.setState({loading: false});
-        }
-      })
-      .catch(error => {
-        CommonFunc.notifyMessage(error.message, error.status);
-        this.setState({loading: false});
-      })
-  }
 
   onTextChange = (event) => {
     let name = event.target.name;
@@ -140,43 +87,24 @@ class AddTask extends Component {
     });
   }
 
-  onTogglePopup = (data, isEdit) => {
+  onTogglePopup = () => {
     this.setState({modelVisible: !this.state.modelVisible})
-    if (isEdit) {
-      this.setState({
-        productId: data.productId,
-        productName: data.productName,
-        description: data.description,
-        unitPrice: data.unitPrice,
-        image: data.image,
-        selectedCategory: data.category,
-        editEnabled: true,
-        src: data.image,
-        qty: data.qty,
-        status: data.status
-      })
-    } else {
-      this.setState({
-        productId: '',
-        productName: '',
-        description: '',
-        unitPrice: '',
-        image: '',
-        selectedCategory: '',
-        src: null,
-        editEnabled: false,
-        qty: 0,
-        status: false
-      })
-      this.setState({editEnabled: false})
-    }
+    this.setState({
+      taskId: '',
+      date: '',
+      description: '',
+      unitPrice: '',
+      image: '',
+      selectedMainFeature: '',
+      editEnabled: false,
+    })
 
   }
 
   onSaveProduct = async () => {
 
-    if (!Validations.textFieldValidator(this.state.productName, 1)) {
-      CommonFunc.notifyMessage('Please enter product name', 0);
+    if (!Validations.textFieldValidator(this.state.date, 1)) {
+      CommonFunc.notifyMessage('Please enter date', 0);
     } else if (!Validations.textFieldValidator(this.state.description, 1)) {
       CommonFunc.notifyMessage('Please enter product description', 0);
     } else if (!Validations.numberValidator(this.state.unitPrice)) {
@@ -185,18 +113,16 @@ class AddTask extends Component {
       CommonFunc.notifyMessage('Please enter product description', 0);
     } else if (!Validations.textFieldValidator(this.state.description, 1)) {
       CommonFunc.notifyMessage('Please enter product description', 0);
-    } else if (this.state.selectedCategory.length === 0) {
-      CommonFunc.notifyMessage('Please select category', 0);
+    } else if (this.state.selectedMainFeature.length === 0) {
+      CommonFunc.notifyMessage('Please select mainFeature', 0);
     } else {
       this.setState({loading: true})
       const data = {
-        name: this.state.productName,
+        date: this.state.date,
         description: this.state.description,
         imageUrl: this.state.src,
         price: this.state.unitPrice,
-        qty: 15,
-        status: true,
-        categoryId: this.state.selectedCategory
+        mainFeature: this.state.selectedMainFeature
       }
       await ProductService.saveProduct(data)
         .then(res => {
@@ -233,69 +159,6 @@ class AddTask extends Component {
       });
   }
 
-  onUpdateProduct = async (item, type) => {
-
-    if (!Validations.textFieldValidator(this.state.productName.trim(), 1)) {
-      CommonFunc.notifyMessage('Please enter product name', 0);
-    } else if (!Validations.textFieldValidator(this.state.description.trim(), 1)) {
-      CommonFunc.notifyMessage('Please enter product description', 0);
-    } else if (!Validations.numberValidator(this.state.unitPrice.toString())) {
-      CommonFunc.notifyMessage('Please enter valid price', 0);
-    } else if (!Validations.textFieldValidator(this.state.description.trim(), 1)) {
-      CommonFunc.notifyMessage('Please enter product description', 0);
-    } else if (!Validations.textFieldValidator(this.state.description.trim(), 1)) {
-      CommonFunc.notifyMessage('Please enter product description', 0);
-    } else if (this.state.selectedCategory.length === 0) {
-      CommonFunc.notifyMessage('Please select category', 0);
-    } else {
-      let data = {}
-
-      if (type === 'STATUS') {
-        data = {
-          id: item.productId,
-          name: item.productName,
-          description: item.description,
-          imageUrl: item.image,
-          price: item.unitPrice,
-          qty: item.qty,
-          status: !item.status,
-          categoryId: item.categoryId
-        }
-      } else {
-        data = {
-          id: this.state.productId,
-          name: this.state.productName,
-          description: this.state.description,
-          imageUrl: this.state.src,
-          price: this.state.unitPrice,
-          qty: this.state.qty,
-          status: this.state.status ? 1 : 0,
-          categoryId: this.state.selectedCategory
-        }
-      }
-      this.setState({loading: true})
-      await ProductService.updateProduct(data)
-        .then(res => {
-          if (res.success) {
-            if (type === undefined) {
-              this.onTogglePopup()
-            }
-            CommonFunc.notifyMessage('Product record has been updated!', 1);
-            this.getAllProducts()
-          } else {
-            CommonFunc.notifyMessage(res.message, 0);
-            this.setState({loading: false})
-          }
-        })
-        .catch(err => {
-          console.log(err)
-          this.setState({loading: false})
-        })
-    }
-
-
-  }
-
   onDeleteProduct = async (id) => {
     this.setState({loading: true})
     await ProductService.deleteProduct(id)
@@ -314,28 +177,8 @@ class AddTask extends Component {
       })
   }
 
-  handleDrop = acceptedFiles => {
-    if (acceptedFiles && acceptedFiles.length > 0) {
-      const reader = new FileReader();
-      reader.addEventListener('load', () =>
-        this.setState({src: reader.result, drop: false, asImageEdit: true})
-      );
-      reader.readAsDataURL(acceptedFiles[0]);
-    }
-  }
-
-  onSelectFile = e => {
-    if (e.target.files && e.target.files.length > 0) {
-      const reader = new FileReader();
-      reader.addEventListener('load', () =>
-        this.setState({src: reader.result, drop: false, asImageEdit: true})
-      );
-      reader.readAsDataURL(e.target.files[0]);
-    }
-  };
-
   render() {
-    const {totalElements, list, searchTxt, modelVisible, loading, editEnabled, asImageEdit, asSearch, drop, dropdownOpen, imgBase64, selectedPage, src, selectedCategory, category, description, productName, unitPrice, involveProjectVisible} = this.state;
+    const {totalElements, list, searchTxt, modelVisible, loading, editEnabled, asSearch, selectedPage, selectedMainFeature, mainFeatures, description, involveProjectVisible, taskType, projects, selectedProject, selectedTaskType,designationTypes,selectedDesignation,hours,minutes,comment} = this.state;
 
     const listData = list.map((items, i) => (
       <tr key={i}>
@@ -345,7 +188,7 @@ class AddTask extends Component {
           <Button color="dark" className="btn-pill shadow" onClick={() => this.onTogglePopup(items, true)}>Add
             Task</Button>
           <Button color="danger" className="btn-pill shadow"
-                  onClick={() => this.deleteHandler(items.productId)}>Edit</Button>
+                  onClick={() => this.deleteHandler(items.taskId)}>Edit</Button>
         </td>
       </tr>
     ));
@@ -395,32 +238,33 @@ class AddTask extends Component {
           </Col>
         </Row>
 
-        <Modal size={'sm'} isOpen={involveProjectVisible} toggle={()=>this.setState({involveProjectVisible:!this.state.involveProjectVisible})}
+        <Modal size={'sm'} isOpen={involveProjectVisible}
+               toggle={() => this.setState({involveProjectVisible: !this.state.involveProjectVisible})}
                className={'modal-lg ' + this.props.className}>
-          <ModalHeader toggle={this.onTogglePopup}>Involved to new Project</ModalHeader>
+          <ModalHeader toggle={() => this.setState({involveProjectVisible: !this.state.involveProjectVisible})}>Involved to new Project</ModalHeader>
           <ModalBody className="pl-5 pr-5">
             <FormGroup row className="pl-5 pr-5">
               <Label>Select the project</Label>
-              <Input type="select" name="selectedCategory" onChange={this.onTextChange}>
-                <option value="" disabled={selectedCategory !== ""}>Please select category</option>
-                {category.map(item => (
-                  <option value={item.value} selected={item.value === selectedCategory}>{item.label}</option>
+              <Input type="select" name="selectedProject" onChange={this.onTextChange}>
+                <option value="" disabled={selectedProject !== ""}>Select Project</option>
+                {projects.map(item => (
+                  <option value={item.value} selected={item.value === selectedProject}>{item.label}</option>
                 ))}
               </Input>
             </FormGroup>
             <FormGroup row className="pl-5 pr-5">
               <Label>Select Designation</Label>
-              <Input type="select" name="selectedCategory" onChange={this.onTextChange}>
-                <option value="" disabled={selectedCategory !== ""}>Please select category</option>
-                {category.map(item => (
-                  <option value={item.value} selected={item.value === selectedCategory}>{item.label}</option>
+              <Input type="select" name="selectedDesignation" onChange={this.onTextChange}>
+                <option value="" disabled={selectedDesignation !== ""}>Select designation</option>
+                {designationTypes.map(item => (
+                  <option value={item.value} selected={item.value === selectedDesignation}>{item.label}</option>
                 ))}
               </Input>
             </FormGroup>
           </ModalBody>
           <ModalFooter>
             <Button color="primary"
-              onClick={()=>this.setState({involveProjectVisible:false})}
+                    onClick={() => this.setState({involveProjectVisible: false})}
             >
               Done
             </Button>
@@ -436,16 +280,16 @@ class AddTask extends Component {
               <FormGroup row>
                 <Label sm={3}>Select the date</Label>
                 <Col sm={4}>
-                  <Input type="date" name="selectedCategory" onChange={this.onTextChange}/>
+                  <Input type="date" name="selectedMainFeature" onChange={this.onTextChange}/>
                 </Col>
               </FormGroup>
               <FormGroup row>
                 <Label sm={3}>Main Features</Label>
                 <Col sm={9}>
-                  <Input type="select" name="selectedCategory" onChange={this.onTextChange}>
-                    <option value="" disabled={selectedCategory !== ""}>Please select category</option>
-                    {category.map(item => (
-                      <option value={item.value} selected={item.value === selectedCategory}>{item.label}</option>
+                  <Input type="select" name="selectedMainFeature" onChange={this.onTextChange}>
+                    <option value="" disabled={selectedMainFeature !== ""}>Please select main feature</option>
+                    {mainFeatures.map(item => (
+                      <option value={item.value} selected={item.value === selectedMainFeature}>{item.label}</option>
                     ))}
                   </Input>
                 </Col>
@@ -463,11 +307,13 @@ class AddTask extends Component {
                 <Label sm={3}>Number of hours</Label>
                 <Row className="pl-3">
                   <Col sm={3}>
-                    <Input type="text" name="hours" placeholder="Hours" value={productName}
+                    <Input type="text" name="hours" placeholder="Hours"
+                           value={hours}
                            onChange={this.onTextChange}/>
                   </Col>
                   <Col sm={3} className="pl-0 ml-0">
-                    <Input type="text" name="minutes" placeholder="Minutes" value={productName}
+                    <Input type="text" name="minutes" placeholder="Minutes"
+                           value={minutes}
                            onChange={this.onTextChange}/>
                   </Col>
                 </Row>
@@ -477,10 +323,10 @@ class AddTask extends Component {
               <FormGroup row>
                 <Label sm={3}>Task type</Label>
                 <Col sm={5}>
-                  <Input type="select" name="selectedCategory" onChange={this.onTextChange}>
-                    <option value="" disabled={selectedCategory !== ""}>Please select category</option>
-                    {category.map(item => (
-                      <option value={item.value} selected={item.value === selectedCategory}>{item.label}</option>
+                  <Input type="select" name="selectedTaskType" onChange={this.onTextChange}>
+                    <option value="" disabled={selectedTaskType !== ""}>Select Task</option>
+                    {taskType.map(item => (
+                      <option value={item.value} selected={item.value === selectedTaskType}>{item.label}</option>
                     ))}
                   </Input>
                 </Col>
@@ -489,7 +335,7 @@ class AddTask extends Component {
               <FormGroup row>
                 <Label sm={3}>Comment</Label>
                 <Col sm={9}>
-                  <Input type="textarea" name="comment" placeholder="Description" value={description}
+                  <Input type="textarea" name="comment" placeholder="Comments" value={description}
                          onChange={this.onTextChange}/>
                 </Col>
               </FormGroup>
@@ -501,7 +347,7 @@ class AddTask extends Component {
           <ModalFooter>
             <Button color="secondary" onClick={this.onTogglePopup}>Cancel</Button>
             <Button color="primary"
-                    onClick={!editEnabled ? this.onSaveProduct : this.onUpdateProduct}>{!editEnabled ? 'Edit' : 'Submit'}</Button>
+                    onClick={this.onSaveProduct}>{'Submit'}</Button>
           </ModalFooter>
         </Modal>
 
