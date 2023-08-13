70559df8417f6ffe88c5c750706486d94902e39f
kavindadilshan <kavindadilshan97@gmail.com>
2023-08-13 14:59:09 +0530

    add popup for view action

--- src/views/TaskHistory/TaskHistory.js
+++ src/views/TaskHistory/TaskHistory.js
@@ -10,13 +10,14 @@ import {
   InputGroup,
   InputGroupAddon, Modal, ModalFooter, ModalHeader,
   Row,
-  Table,Label,Alert
+  Table, Label, Alert, ModalBody, Form, FormGroup
 } from "reactstrap";
 import './TaskHistory.scss';
 import * as ProductService from '../../services/product';
 import * as CommonFunc from "../../utils/CommonFunc";
 import Loader from "../../components/Loader/loading";
 import {DateRangePickerComponent} from '@syncfusion/ej2-react-calendars';
+import {DesignationTypes, ProjectsList, TaskType} from "../../constance/Constance";
 
 class TaskHistory extends Component {
   state = {
@@ -40,7 +41,22 @@ class TaskHistory extends Component {
         hours:'5.30'
       }
     ],
-    loading: true,
+    loading: false,
+    modelVisible: false,
+    taskId: '',
+    date: '',
+    description: '',
+    hours: '',
+    minutes: '',
+    comment: '',
+    taskType: TaskType,
+    projects: ProjectsList,
+    designationTypes: DesignationTypes,
+    mainFeatures: [],
+    selectedTaskType: '',
+    selectedProject: '',
+    selectedDesignation: '',
+    selectedMainFeature: '',
   }
 
   async componentDidMount() {
@@ -74,8 +90,25 @@ class TaskHistory extends Component {
       })
   }
 
+  onTogglePopup = (item) => {
+    this.setState({modelVisible: !this.state.modelVisible})
+    this.setState({
+      date: '',
+      selectedMainFeature: '',
+      description: '',
+      hours: '',
+      minutes: '',
+      selectedTaskType: '',
+      comment:'',
+    })
+    if (item){
+      this.setState({selectedProject:item})
+    }
+
+  }
+
   render() {
-    const {list, loading} = this.state;
+    const {list, loading, comment, date, description, designationTypes, hours, mainFeatures, minutes, modelVisible, projects, selectedDesignation, selectedMainFeature, selectedProject, selectedTaskType, taskId, taskType } = this.state;
     const listData = list.map((items, i) => (
       <tr key={i}>
         <td>{items.date}</td>
@@ -86,7 +119,7 @@ class TaskHistory extends Component {
         <td>{items.hours}</td>
         <td className={'btn-align'}>
           <Button color="success" className="btn-pill shadow"
-            // onClick={() => this.onTogglePopup(items)}
+            onClick={() => this.onTogglePopup(items)}
           >View More</Button>
         </td>
       </tr>
@@ -174,6 +207,86 @@ class TaskHistory extends Component {
             </Alert>
           </Col>
         </Row>
+
+        <Modal isOpen={modelVisible} toggle={this.onTogglePopup}
+               className={'modal-lg ' + this.props.className}>
+          <ModalHeader toggle={this.onTogglePopup}>{`Project Name : ${selectedProject.projectName}`}</ModalHeader>
+
+          <ModalBody className="pl-5 pr-5">
+            <Form>
+              <FormGroup row>
+                <Label sm={3}>Select the date</Label>
+                <Col sm={4}>
+                  <Input type="date" name="date" onChange={this.onTextChange}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label sm={3}>Main Features</Label>
+                <Col sm={9}>
+                  <Input type="select" name="selectedMainFeature" onChange={this.onTextChange}>
+                    <option value="" disabled={selectedMainFeature !== ""}>Please select main feature</option>
+                    {mainFeatures.map(item => (
+                      <option value={item.value} selected={item.value === selectedMainFeature}>{item.label}</option>
+                    ))}
+                  </Input>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={3}>Task description</Label>
+                <Col sm={9}>
+                  <Input type="textarea" name="description" placeholder="Description" value={description}
+                         onChange={this.onTextChange}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={3}>Number of hours</Label>
+                <Row className="pl-3">
+                  <Col sm={3}>
+                    <Input type="number" name="hours" placeholder="Hours"
+                           value={hours}
+                           onChange={this.onTextChange}/>
+                  </Col>
+                  <Col sm={3} className="pl-0 ml-0">
+                    <Input type="number" name="minutes" placeholder="Minutes"
+                           value={minutes}
+                           onChange={this.onTextChange}/>
+                  </Col>
+                </Row>
+
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={3}>Task type</Label>
+                <Col sm={5}>
+                  <Input type="select" name="selectedTaskType" onChange={this.onTextChange}>
+                    <option value="" disabled={selectedTaskType !== ""}>Select Task</option>
+                    {taskType.map(item => (
+                      <option value={item.value} selected={item.value === selectedTaskType}>{item.label}</option>
+                    ))}
+                  </Input>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label sm={3}>Comment</Label>
+                <Col sm={9}>
+                  <Input type="textarea" name="comment" placeholder="Comments" value={comment}
+                         onChange={this.onTextChange}/>
+                </Col>
+              </FormGroup>
+            </Form>
+
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="secondary" onClick={this.onTogglePopup}>Cancel</Button>
+            <Button color="primary"
+                    onClick={this.onSaveProduct}>{'Submit'}</Button>
+          </ModalFooter>
+        </Modal>
 
         <Loader
           asLoading={loading}
